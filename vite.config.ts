4e0ccd82b2bb50619f0a793fdc0c59b481eecf41
OptimisticLife <134665728+OptimisticLife@users.noreply.github.com>
2025-05-09 09:05:11 +0530

    config updated for remote host

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  // const isDev = mode === "development";
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
 
   return {
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: "https://stream-app-server-auj8.onrender.com", // Your Node proxy server
+          target: isDev
+            ? "http://localhost:4647"
+            : "https://stream-app-server-auj8.onrender.com", // Your Node proxy server
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
   };
-  // {
-  //   plugins: [react()],
-  //   server: {
-  //     proxy: {
-  //       "/api": {
-  //         target: isDev
-  //           ? "http://localhost:4647"
-  //           : "https://stream-app-server-auj8.onrender.com", // Your Node proxy server
-  //         changeOrigin: true,
-  //         rewrite: (path) => path.replace(/^\/api/, ""),
-  //       },
-  //     },
-  //   },
-  // };
 });
-// import.meta.env.DEV
